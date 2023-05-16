82ede58ac6c189a27c5142009f2fa1bb8580ef2a
- <-@DESKTOP-5LUH4K3>
2023-05-16 07:52:58 +0300

    arrays new task

--- Arrays/Source.cpp
+++ Arrays/Source.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ï»¿#include <iostream>
 using namespace std;
 
 //#define ARR_2
@@ -19,122 +19,46 @@ void main()
 	cout << arr[2] << endl;
 #endif // ARR_2
 
-#ifdef ARR_3
 	const int SIZE = 5;
 	int arr[SIZE];
 
 	for (int i = 0; i < SIZE; i++)
 	{
-		cout << "Ââåäèòå ìàññèâ: "; cin >> arr[i];
+		cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ Ð¼Ð°ÑÑÐ¸Ð²: "; cin >> arr[i];
 
 	}
 	for (int i = 0; i < SIZE; i++)
 	{
-		cout << "ìàññèâ " << i << " = " << arr[i] << "\t";
+		cout << "Ð¼Ð°ÑÑÐ¸Ð² " << i << " = " << arr[i] << "\t";
 	}
-#endif // ARR_3
-
-
-#ifdef ARR_4
-	const int SIZE = 5;
-	int arr[SIZE];
-
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << "Ââåäèòå ìàññèâ: "; cin >> arr[i];
-
-	}
-	cout << endl;
 
 	for (int i = SIZE - 1; i >= 0; i--)
 	{
 		cout << arr[i] << "\t";
 
 	}
-#endif // ARR_4
 
-#ifdef ARR_5
-	const int SIZE = 5;
-	int arr[SIZE];
 	int sum = 0;
-
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << "Ââåäèòå ìàññèâ: "; cin >> arr[i];
-	}
-	cout << endl;
-
 	for (int i = SIZE - 1; i >= 0; i--)
 	{
 		sum += arr[i];
 	}
-	cout << sum << endl;
+	cout << "Ð¡ÑƒÐ¼Ð¼Ð° ÑÐ»ÐµÐ¼ÐµÐ½Ñ‚Ð¾Ð² Ð¼Ð°ÑÑÐ¸Ð²Ð°: " << sum << endl;
 
-#endif // ARR_5
+	cout << "Ð¡Ñ€ÐµÐ´Ð½ÐµÐµ-Ð°Ñ€Ð¸Ñ„Ð¼ÐµÑ‚Ð¸Ñ‡ÐµÑÐºÐ¾Ðµ: " << (double)sum / SIZE << endl;
 
-
-#ifdef ARR_6
-	const int SIZE = 5;
-	int arr[SIZE];
-	int sum = 0;
-
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << "Ââåäèòå ìàññèâ: "; cin >> arr[i];
-	}
-	cout << endl;
-
-	for (int i = SIZE - 1; i >= 0; i--)
-	{
-		sum += arr[i];
-	}
-	cout << sum / SIZE << endl;
-#endif // ARR_6
-
-
-#ifdef ARR_7
-	const int SIZE = 5;
-	int arr[SIZE];
-	int max = 0;
-
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << "Ââåäèòå ìàññèâ: "; cin >> arr[i];
-	}
-	cout << endl;
-
-	
+	int min, max;
+	min = max = arr[0];
 	for (int i = SIZE - 1; i >= 0; i--)
 	{
 		if (arr[i] > max)
 			max = arr[i];
 	}
 	cout << max << endl;
-#endif // ARR_7
-
-
-#ifdef ARR_8
-	const int SIZE = 5;
-	int arr[SIZE];
-	int min = 2147483647;
-
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << "Ââåäèòå ìàññèâ: "; cin >> arr[i];
-	}
-	cout << endl;
-
 	for (int i = SIZE - 1; i >= 0; i--)
 	{
 		if (arr[i] < min)
 			min = arr[i];
 	}
 	cout << min << endl;
-#endif // ARR_8
-
-
-
 }
-	
-
-
---
+++ SHIFT/Source.cpp
@@ -0,0 +1,62 @@
+﻿#include<iostream>
+using namespace std;
+#define tab "\t"
+//#define SHIFTPLUS
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n] = { 0,1,2,3,4,5,6,7,8,9 };
+	
+
+	
+#ifdef SHIFTPLUS
+
+	//вывод исходного массива на экран:
+	int number_of_shift;
+	cout << "Введите количество сдвигов: "; cin >> number_of_shift;
+	for (int i = 0; i < number_of_shift; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+
+			arr[i] = arr[i + 1];
+		}
+
+		arr[n - 1] = buffer;
+	}
+	//вывод сдвинутого массива
+	for (int i = 0; i < n; i++)
+	{
+
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+#endif // SHIFTPLUS
+
+	int number_of_shift;
+	cout << "Введите количество сдвигов: "; cin >> number_of_shift;
+	for (int i = 0; i < number_of_shift; i++)
+	{
+		int buffer = arr[n-1];
+		for (int i = n-2; i >= 0; i--)
+		{
+
+			arr[i+1] = arr[i];
+		}
+
+		arr[0] = buffer;
+	}
+	//вывод сдвинутого массива
+	for (int i = 0; i < n; i++)
+	{
+
+		cout << arr[i] << tab;
+	}
+	cout << endl; 
+
+
+}
+
